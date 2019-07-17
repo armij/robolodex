c0126e2770dc981bdaa37eb0a9148211f46eefd9
armij <arvilson@sokolabs.co>
2019-07-17 21:57:49 +0800

    Updates

---
+++ precache-manifest.9f1d4dc156d7f320497c537afe8d3f2d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5703ad77a96b3561d78d159cce267bf6",
+    "url": "/robolodex/index.html"
+  },
+  {
+    "revision": "22d3d068672c7a348628",
+    "url": "/robolodex/static/css/main.e6f1454d.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/robolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "22d3d068672c7a348628",
+    "url": "/robolodex/static/js/main.cc6eb4e8.chunk.js"
+  },
+  {
+    "revision": "cd8145f0c7a5e7ef81b7",
+    "url": "/robolodex/static/js/runtime~main.1c8908bc.js"
+  }
+]);
